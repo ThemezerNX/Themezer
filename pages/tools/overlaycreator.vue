0bb0e92c6cacf2e3f16e9da0c546dc1b470de141
Migushthe2nd <ihaverkamp@live.nl>
2020-08-16 21:18:30 +0200

    Update strings

--- pages/tools/overlaycreator.vue
+++ pages/tools/overlaycreator.vue
@@ -17,10 +17,16 @@
 				Upload your layout here. You will get two NXThemes: one with a
 				black background and one with a white one (be sure to allow the
 				site to download multiple files). For each do the following:
-				install it on your Switch and take a screenshot. Do NOT change
-				the system theme and have the same item selected when taking the
-				screenshots. Transfer the screenshots to your PC afterwards.
+				install it on your Switch and take a screenshot. Set the system
+				theme to white(!) and have the same item selected when taking
+				the screenshots. Transfer the screenshots to your PC afterwards.
 				Have you done that? Go to step 2.
+				<a
+					href="https://github.com/ThemezerNX/Layouts#3-creating-overlays"
+					target="_blank"
+				>
+					Click more detailed instructions
+				</a>
 			</div>
 			<v-row class="ma-0">
 				<v-col cols="12" class="pa-2">
