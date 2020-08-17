b74583b1bb9963a7ec4dca303545ec222295a581
Migushthe2nd <ihaverkamp@live.nl>
2020-08-17 11:59:06 +0200

    Redirect to '/' when no id on '/creators/:id'

---
+++ pages/creators/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div />
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+	middleware: ['auth'],
+	options: {
+		auth: true
+	},
+	beforeRouteEnter(_to, _from, next) {
+		next('/')
+	}
+})
+</script>
