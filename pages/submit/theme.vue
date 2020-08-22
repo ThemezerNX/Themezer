de68acca3c32960bd8f9cbba1222e731bc63f823
Migushthe2nd <ihaverkamp@live.nl>
2020-08-22 23:03:06 +0200

    Add extra layouts to dropdown for legacy support

--- pages/submit/theme.vue
+++ pages/submit/theme.vue
@@ -556,18 +556,79 @@ export default Vue.extend({
 			},
 			update(data) {
 				if (data?.layoutList) {
-					return data.layoutList
-						.map((l) => {
-							return {
-								text: `'${l.details.name}' by ${l.creator.display_name}`,
-								value: l.id
-							}
+					const layouts = data.layoutList.map((l) => {
+						return {
+							text: `'${l.details.name}' by ${l.creator.display_name}`,
+							value: l.id
+						}
+					})
+
+					// Add some extra very common layouts
+					if (this.currentThemeTarget === 'ResidentMenu') {
+						layouts.push({
+							text: `'Doge Layout Rounded' by ${
+								data.layoutList.find(
+									(l) => l.creator.id === '249186838592487425'
+								).creator.display_name
+							}`,
+							value: 'e|7638bb49-7475-4d11-a76a-ec9d63b12b94'
 						})
-						.sort((a, b) =>
-							a.text
-								.toLowerCase()
-								.localeCompare(b.text.toLowerCase())
-						)
+						layouts.push({
+							text: `'DogeLayout - M-Edition Rounded' by ${
+								data.layoutList.find(
+									(l) => l.creator.id === '123859829453357056'
+								).creator.display_name
+							}`,
+							value: '4|c4c1239e-a3ec-43b9-835a-49f6220aa222'
+						})
+						layouts.push({
+							text: `'Small Compact Homescreen Rounded' by ${
+								data.layoutList.find(
+									(l) => l.creator.id === '155500696693768202'
+								).creator.display_name
+							}`,
+							value: 'f|e818c9af-ca39-48cd-8ed1-a979b69ea142'
+						})
+					} else if (this.currentThemeTarget === 'Flaunch') {
+						layouts.push({
+							text: `'All Apps 90% Scale Rounded' by ${
+								data.layoutList.find(
+									(l) => l.creator.id === '249186838592487425'
+								).creator.display_name
+							}`,
+							value: 'b|2ee3f0bc-9140-402e-bdfc-d50042c4d45e'
+						})
+					} else if (this.currentThemeTarget === 'Psl') {
+						layouts.push({
+							text: `'Transparent Playerselect 90% Scale Centered' by ${
+								data.layoutList.find(
+									(l) => l.creator.id === '123859829453357056'
+								).creator.display_name
+							}`,
+							value:
+								'3|b77b434f-5811-42fc-bd5e-ab44d7f24b61,d22d7557-99da-4710-a2d8-0bb3c7bfac14'
+						})
+						layouts.push({
+							text: `'Transparent Playerselect 90% Scale' by ${
+								data.layoutList.find(
+									(l) => l.creator.id === '123859829453357056'
+								).creator.display_name
+							}`,
+							value: '3|b77b434f-5811-42fc-bd5e-ab44d7f24b61'
+						})
+						layouts.push({
+							text: `'Transparent Playerselect Centered' by ${
+								data.layoutList.find(
+									(l) => l.creator.id === '123859829453357056'
+								).creator.display_name
+							}`,
+							value: '3|d22d7557-99da-4710-a2d8-0bb3c7bfac14'
+						})
+					}
+
+					return layouts.sort((a, b) =>
+						a.text.toLowerCase().localeCompare(b.text.toLowerCase())
+					)
 				} else return []
 			},
 			error(e) {
