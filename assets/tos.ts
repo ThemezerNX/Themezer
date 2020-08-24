806ee398bc761c99ae9eb3102baa35074086dd61
Migushthe2nd <ihaverkamp@live.nl>
2020-08-24 13:04:22 +0200

    Update TOS

--- assets/tos.ts
+++ assets/tos.ts
@@ -2,7 +2,7 @@ export default [
 	{
 		title: 'Ownership',
 		content:
-			"The uploader remains ownership of files uploaded to Themezer. Files will reside on Themezer's servers, under care of the user, and remain the uploader's responsibility."
+			"The uploader keeps ownership of files uploaded to Themezer. Files will reside on Themezer's servers, under care of the user, and remain the uploader's responsibility."
 	},
 	{
 		title: 'Copyright',
@@ -15,8 +15,13 @@ export default [
 			'Any attempt to exploit, spam or cause harm in any other way will result in immediate termination of your account. Bugs and issues should be reported via on the Contact page or GitHub repository.'
 	},
 	{
-		title: 'Account termination',
+		title: 'NSFW',
 		content:
 			"Themezer reserves every right to terminate a user's account, even without an explanation."
+	},
+	{
+		title: 'Account termination',
+		content:
+			"Themezer reserves every right to terminate a user's account if found necessary, even without an explanation."
 	}
 ]
